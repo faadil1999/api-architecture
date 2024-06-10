49efb10e6f36857df58e44ce776b135078721170
torou_faadil <faadil@lynx-business.com>
2024-06-10 18:21:48 +0200

    feat(use-case): add add-one, delete generation

--- template/model-template/use-cases/generate-get-all.js
+++ template/model-template/use-cases/generate-get-all.js
@@ -3,23 +3,23 @@ const { pluralWord } = require("../../../helper");
 
 function generateGetAllCase(entityName) {
   return `
-    import { ${capitalizeFirstLetter(entityName)} } from '../../domaines/types'
-    import { I${capitalizeFirstLetter(
-      entityName
-    )}Repository } from '../../infrastructure'
+import { ${capitalizeFirstLetter(entityName)} } from '../../domaines/types'
+import { I${capitalizeFirstLetter(
+    entityName
+  )}Repository } from '../../infrastructure'
 
-    export class Get${capitalizeFirstLetter(entityName)}UseCase {
-        constructor(private ${capitalizeFirstLetter(
-          entityName
-        )}Repository: I${capitalizeFirstLetter(entityName)}Repository)
+  export class Get${capitalizeFirstLetter(entityName)}UseCase {
+      constructor(private ${capitalizeFirstLetter(
+        entityName
+      )}Repository: I${capitalizeFirstLetter(entityName)}Repository)
         {
 
         }
-        async execute(): Promise<${capitalizeFirstLetter(entityName)}[]> {
-            return this.${capitalizeFirstLetter(
-              entityName
-            )}Repository.getAll.${pluralWord(entityName)}
-        }
+      async execute(): Promise<${capitalizeFirstLetter(entityName)}[]> {
+         return this.${capitalizeFirstLetter(
+           entityName
+         )}Repository.getAll.${pluralWord(entityName)}
+      }
     }
 
     `;
